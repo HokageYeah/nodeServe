aba89177d15b02610aff04c9b511db3f4ed9312f
yuye <18838280615@163.com>
2023-04-18 11:48:15 +0800

    1、优化增改权限校验方案。 2、回复评论接口逻辑。

--- src/middleware/permission-middleware.ts
+++ src/middleware/permission-middleware.ts
@@ -5,7 +5,7 @@ import { UNGET_USER_INFORMATION, OPERATION_IS_NOT_ALLOWED, MODIFY_USER_MOMENT_ER
 import Permission_DBService from '@/service/permission_service'
 import permission_service from "@/service/permission_service";
 // 用户动态接口增删改茶权限（moment接口权限） 函数柯力化封装
-export const  verifyPermission = (verifyStr: string) => {
+export const  verifyPermission = (verifyStr: string[]) => {
     return  async (
         req: AuthenticatedRequest,
         res: Response,
@@ -19,14 +19,17 @@ export const  verifyPermission = (verifyStr: string) => {
             await next({ code: UNGET_USER_INFORMATION });
             return;
           }
-          const verify = req.body[verifyStr];
-          tokenUserInfo[verifyStr] = verify
-          const [queryMoment] = await permission_service.queryMoment(tokenUserInfo, verifyStr);
+          verifyStr.forEach((element :string) => {
+            const verify = req.body[element];
+            tokenUserInfo[element] = verify
+          });
+          tokenUserInfo['verifyStr'] = verifyStr
+          const [queryMoment] = await permission_service.queryMoment(tokenUserInfo);
           const queryMomentAry: any[] = Array.isArray(queryMoment) ? queryMoment : [queryMoment];
           if (!queryMomentAry || queryMomentAry.length == 0) {
             return await next({ code: MODIFY_USER_MOMENT_ERROR })
           }
-          const [verifyPermission] = await Permission_DBService.checkMomentPermission(tokenUserInfo, verifyStr);
+          const [verifyPermission] = await Permission_DBService.checkMomentPermission(tokenUserInfo);
           const verifyPermissionAry: any[] = Array.isArray(verifyPermission) ? verifyPermission : [verifyPermission];
           if (!verifyPermissionAry || verifyPermissionAry.length == 0) {
               return await next({ code: OPERATION_IS_NOT_ALLOWED })
--- src/service/permission_service.ts
+++ src/service/permission_service.ts
@@ -4,31 +4,48 @@ import connectionMysql from "@/tools/mysql_db";
 import { User } from "@/libcommon/index";
 interface Permission_DBServiceCls<T extends User> {
     // 查询数据是否存在
-    queryMoment(user: T, verifyPermiss: string): Promise<[RowDataPacket[] | RowDataPacket[][] | OkPacket | OkPacket[] | ResultSetHeader, FieldPacket[]]>;
+    queryMoment(user: T): Promise<[RowDataPacket[] | RowDataPacket[][] | OkPacket | OkPacket[] | ResultSetHeader, FieldPacket[]]>;
     // 验证动态接口的删改当前用户是否有权限（只能修改自己的动态）
     checkMomentPermission(user: T, verifyPermiss: string): Promise<[RowDataPacket[] | RowDataPacket[][] | OkPacket | OkPacket[] | ResultSetHeader, FieldPacket[]]>;
 }
 class Permission_DBService<T extends User> implements Permission_DBServiceCls<T>{
-    async queryMoment(user: T, verifyPermiss: string): Promise<[ResultSetHeader | RowDataPacket[] | RowDataPacket[][] | OkPacket | OkPacket[], FieldPacket[]]> {
+    async queryMoment(user: T): Promise<[ResultSetHeader | RowDataPacket[] | RowDataPacket[][] | OkPacket | OkPacket[], FieldPacket[]]> {
         const connect = await connectionMysql();
         // 获取用户信息
-        const verifyPermissStr = user[verifyPermiss];
+        const verifyPermissAry = user['verifyStr'];
+        // 要求传递的数组，第一个必须是表的主键，切主键中包含标名称：表名命名规则user_主键名(去掉id)
+        const verifyPermiss = verifyPermissAry[0]
         const table = 'user_' + verifyPermiss.replace('id','');
+        let sqlStr = `SELECT * FROM ${table} WHERE `
+        let sqlAry: any[] = [];
+        verifyPermissAry.forEach((element: string) => {
+            sqlStr += `${element} = ?`
+            sqlAry.push(user[element]);
+        });
         // 拼接sql语句
-        const sql = `SELECT * FROM ${table} WHERE ${verifyPermiss} = ?`;
-        const createMoment = await connect.execute<ResultSetHeader>(sql, [verifyPermissStr]);
+        // const sql = `SELECT * FROM ${table} WHERE ${verifyPermiss} = ?`;
+        console.log("tokenUserInfo===========>",sqlStr, sqlAry);
+        const createMoment = await connect.execute<ResultSetHeader>(sqlStr, sqlAry);
         connect.release();
         return createMoment;
     }
-    async checkMomentPermission(user: T, verifyPermiss: string): Promise<[OkPacket | ResultSetHeader | RowDataPacket[] | RowDataPacket[][] | OkPacket[], FieldPacket[]]> {
+    async checkMomentPermission(user: T): Promise<[OkPacket | ResultSetHeader | RowDataPacket[] | RowDataPacket[][] | OkPacket[], FieldPacket[]]> {
         const connect = await connectionMysql();
         // 获取用户信息
         const { userid } = user as User;
-        const verifyPermissStr = user[verifyPermiss];
+        // 获取用户信息
+        const verifyPermissAry = user['verifyStr'];
+        const verifyPermiss = verifyPermissAry[0]
         const table = 'user_' + verifyPermiss.replace('id','');
+        let sqlStr = `SELECT * FROM ${table} WHERE user_id = ?`
+        let sqlAry: any[] = [];
+        verifyPermissAry.forEach((element: string) => {
+            sqlStr += `AND ${element} = ?`
+            sqlAry.push(user[element]);
+        });
         // 拼接sql语句
         const sql = `SELECT * FROM ${table} WHERE user_id = ? AND ${verifyPermiss} = ?`;
-        const createMoment = await connect.execute<ResultSetHeader>(sql, [userid, verifyPermissStr]);
+        const createMoment = await connect.execute<ResultSetHeader>(sql, [userid, ...sqlAry]);
         connect.release();
         return createMoment;
     }

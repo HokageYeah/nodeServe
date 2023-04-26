c2e36dea787c7e3d50d20e149909baeff268e0ff
yuye <18838280615@163.com>
2023-04-26 18:39:17 +0800

    1、websocket 库ws 下载。 2、与客户端简历socket连接，实现数据动态实时更新。

---
+++ src/app/WebSocket.ts
@@ -0,0 +1,47 @@
+// webscoket中间件 启动连接
+import WebSocket from "ws";
+import { Server as ServerHttp } from "http";
+import { Server as ServerWs } from "ws";
+
+class WebSocketServer {
+  // 保存所有连接的客户端
+  clients: WebSocket[];
+  private wsServer: ServerWs;
+  constructor(server: ServerHttp) {
+    // 创建 WebSocket 服务器并将其连接到 HTTP 服务器上
+    this.wsServer = new WebSocket.Server({ server });
+    this.clients = [];
+    const that = this;
+
+    // 监听 WebSocket 连接事件
+    this.wsServer.on("connection", function (socket: WebSocket) {
+      // 将客户端添加到clients中
+      that.clients.push(socket);
+      // 向客户端发送消息
+      socket.send("<h1>你好客户端，我是服务器的哈哈哈哈消息</h1>");
+      // 监听客户端发来的消息
+      socket.on("message", (message: string) => {
+        console.log(`WebSocket 客户端发送过来的消息: ${message}`);
+      });
+      // 监听 WebSocket 连接关闭事件
+      socket.on("close", () => {
+        console.log("WebSocket 连接关闭了");
+      });
+    });
+
+    // 定时向所有客户端发送消息
+    setInterval(() => {
+      const data = new Date().toString();
+      console.log("发送消息：", data);
+
+      // 遍历所有客户端并发送消息
+      this.clients.forEach((client) => {
+        // 判断是否处于连接上的
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(data);
+        }
+      });
+    }, 10000);
+  }
+}
+export default WebSocketServer;
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,28 @@
+import app from "@/app/index";
+import { SERVER_PORT } from "@/config/server";
+import WebSocketServer from '@/app/WebSocket'
 
-import app from '@/app/index'
-import { SERVER_PORT } from "@/config/server"
-
-app.listen(SERVER_PORT, () => {
+// 创建 HTTP 服务器并将 Express 应用程序挂载到它上面
+const server = app.listen(SERVER_PORT, () => {
   console.log("app.js入口服务启动了");
 });
+// 创建 WebSocket 服务
+const wsServer = new WebSocketServer(server);
+
+// // 创建 WebSocket 服务器并将其连接到 HTTP 服务器上
+// const wsServer = new WebSocket.Server({ server });
+
+// // 监听 WebSocket 连接事件
+// wsServer.on("connection", function (socket: WebSocket) {
+//   console.log("WebSocket 已经连接上了=====>", socket);
+//   // 向客户端发送消息
+//   socket.send("<h1>你好客户端，我是服务器的消息</h1>");
+//   // 监听客户端发来的消息
+//   socket.on("message", (message: string) => {
+//     console.log(`WebSocket 客户端发送过来的消息: ${message}`);
+//   });
+//   // 监听 WebSocket 连接关闭事件
+//   socket.on("close", () => {
+//     console.log("WebSocket 连接关闭了");
+//   });
+// });
